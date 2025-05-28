bdeb257cba1d3fa10f165767366d9354efe8428c
Ming Chi <ming@state.com>
2025-05-28 23:37:26 +0100

    Logout form dashboarded implemented

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -3,15 +3,23 @@ import { useAuth } from "../../component/context/AuthContext";
 
 function Dashboard() {
   const { user, logout } = useAuth();
- 
+  const navigate = useNavigate()
 if (!user) {
     return <Navigate to="/login" replace />;
+  }
+
+  const handleLogout = ()=>{
+    
+    logout()
+    navigate("/login")
   }
   return (
     <div>
       <h2>Dashboard</h2>
       <p>Welcome {user.name}</p>
       <p>{user.email}</p>
+      <button onClick={handleLogout}>Logout</button>
+
       {/* <button onClick={handleLogout}>logout</button> */}
     </div>
   );
