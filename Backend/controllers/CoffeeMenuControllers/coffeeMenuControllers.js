5ba79266eabc9f3e83c3789aa8a84e4de19c014b
Ming Chi <ming@state.com>
2025-07-19 00:01:27 +0100

    coffee menu controller error status updated

--- Backend/controllers/CoffeeMenuControllers/coffeeMenuControllers.js
+++ Backend/controllers/CoffeeMenuControllers/coffeeMenuControllers.js
@@ -1,19 +1,29 @@
 import { supabase } from "../../lib/supaBaseClient.js";
 
 const coffeeMenuControllers = async (req, res) => {
+
   const { data, error } = await supabase
     .from("coffee")
-    .select("title, description, ingredients");
+    .select("*");
+
+    console.log("Startup data:", data);
 
   if (error) {
+    console.log("❌ Error fetching coffee data:", error.message);
     return res.status(500).json({
       error: error.message,
     });
   }
 
+  if (data && data.length > 0) {
+    console.log(`✅ Successfully fetched ${data.length} coffee records.`);
+  } else {
+    console.log("⚠️ No coffee data found.");
+  }
+
   res.status(200).json({
-    coffee: data
+    coffee: data,
   });
 };
 
-export default coffeeMenuControllers
+export default coffeeMenuControllers;
